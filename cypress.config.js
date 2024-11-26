3fc9a804a0af7481d2c1f57d4eb7eb6c97c20dc8
Dina Kazakevich <29330744+dinakazakevich@users.noreply.github.com>
2024-11-27 00:08:24 +0400

    Added spec path

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
