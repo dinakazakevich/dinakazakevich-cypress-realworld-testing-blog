e1b72650f218c73d6f53774cd28e48b918cc45e0
Dina Kazakevich <29330744+dinakazakevich@users.noreply.github.com>
2024-11-27 00:11:22 +0400

    Remove support file mention

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/e2e.ts",
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
